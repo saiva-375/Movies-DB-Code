8dd659c624271fe3ca701a6d89a838e6f2af4533
saiva-375 <saivadlamudi375@gmail.com>
2026-01-29 13:54:29 -0500

    Initial Commit

---
+++ Movies DB Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies DB Code
+//
+//  Created by seshi on 1/29/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movies DB CodeUITests/Movies_DB_CodeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Movies_DB_CodeUITestsLaunchTests.swift
+//  Movies DB CodeUITests
+//
+//  Created by seshi on 1/29/26.
+//
+
+import XCTest
+
+final class Movies_DB_CodeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
